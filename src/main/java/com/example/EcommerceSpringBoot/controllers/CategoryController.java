3d72a5ad2fa714119acf63e6e38de00e5d6f552e
Shayan <shayanyusuf143@gmail.com>
2025-06-24 21:35:26 +0530

    Added controller and service layer

---
+++ src/main/java/com/example/EcommerceSpringBoot/controllers/CategoryController.java
@@ -0,0 +1,9 @@
+package com.example.EcommerceSpringBoot.controllers;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController {
+
+}
---
+++ src/main/java/com/example/EcommerceSpringBoot/services/CategoryService.java
@@ -0,0 +1,7 @@
+package com.example.EcommerceSpringBoot.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+}
